3053d0417e9055e56d63863ef47705846fc4acfe
DinithiG <dinithi.20210465@iit.ac.lk>
2023-02-26 17:58:31 +0530

    new code

---
+++ masf-back-end/backend/tensorflow.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+
+print(tf.__version__)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+# Make NumPy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
