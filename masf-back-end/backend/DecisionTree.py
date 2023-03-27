b98713db06f36a359c1d739e7a29d71fc90e88ea
DinithiG <dinithi.20210465@iit.ac.lk>
2023-03-27 06:49:09 +0530

    dt

--- masf-back-end/backend/DecisionTree.py
+++ masf-back-end/backend/DecisionTree.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 # Load the CSV file into a Pandas DataFrame
-df = pd.read_csv("googleplaystore.csv")
+df = pd.read_csv("/Users/firefly0118/Documents/uni/L5 - second year/semester 1/SDGP/Implementation/masf-back-end/backend/googleplaystore.csv")
 
 # Split the DataFrame into input features (X) and target variable (y)
-X = df.drop("target_variable_name", axis=1)  # Replace "target_variable_name" with the name of your target variable column
-y = df["target_variable_name"]  # Replace "target_variable_name" with the name of your target variable column
+X = df.drop("Installs", axis=1)  # Replace "target_variable_name" with the name of your target variable column
+y = df["Installs"]  # Replace "target_variable_name" with the name of your target variable column
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
