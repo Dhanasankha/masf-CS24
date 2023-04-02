36b814abffd7c92fffb459ac45f2137a07c9c246
Shanuka01 <Shanuka.20210252@iit.ac.lk>
2023-04-02 20:27:51 +0530

    creat SignUp page

---
+++ masf-front-end/src/components/SignUpSection/SignUpSection.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import '../App.css';
+import './SignUpSection.css'
+import { Button } from './Button';
+
+function SignUpSection() {
+  return (
+    <div className='SignUp-container'>
+
+    <video src='/videos/video2.mp4' autoPlay loop muted />
+
+        <h1>Sign Up</h1>
+      
+
+        <div className='input-areas'>
+            <form>
+                <input
+                className='SignUp-inputs'
+                name='Name'
+                type='name'
+                placeholder='Your Name'
+                />
+            </form>
+
+            <form>
+                <input
+                className='SignUp-inputs'
+                name='Email'
+                type='email'
+                placeholder='Your Email'
+                />
+            </form>
+
+            <form>
+                <input
+                className='SignUp-inputs'
+                name='Password'
+                type='password'
+                placeholder='Your Password'
+                />
+            </form>
+
+            <form>
+                <input
+                className='SignUp-inputs'
+                name='Password'
+                type='password'
+                placeholder='Confirm Password'
+                />
+            </form>
+            <Button buttonStyle='btn--outline'>SIGN UP</Button>
+            </div>
+        
+    </div>
+  );
+}
+
+export default SignUpSection;
---
+++ masf-front-end/src/view/SignUpPage/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import SignUpSection from '../SignUpSection'
+import Footer from '../Footer';
+
+function SignUp() {
+  return (
+    <>
+      <SignUpSection />
+      <Footer />
+    </>
+  );
+}
+
+export default SignUp;
